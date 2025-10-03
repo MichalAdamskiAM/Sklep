e33c52688d570d9b5ab723b7a0978bb783335994
Michał Adamski <adamski.michal.am@gmail.com>
2025-10-03 10:43:18 +0200

    Klient class

--- Sklep/Sklep/Program.cs
+++ Sklep/Sklep/Program.cs
@@ -4,9 +4,38 @@ namespace Sklep
 {
     class Program
     {
+        class Klient
+        {
+            private string email;
+            private string haslo;
+
+            public Klient(string email, string haslo)
+            {
+                this.email = email;
+                this.haslo = haslo;
+            }
+
+
+        }
+
+        class Lista_klientow
+        {
+            private List<Klient> klienci;
+
+            public Lista_klientow()
+            {
+                klienci = new List<Klient>();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //
+            //costam@gmail.com; maslo; chleb
+            //inny@gmail.com; suszarka; dzem; 
+
+
         }
     }
 }
