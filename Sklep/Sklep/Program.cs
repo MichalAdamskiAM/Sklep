fd132f40d1ec4596642b11079409c3723061c667
Michał Adamski <adamski.michal.am@gmail.com>
2025-11-14 11:12:17 +0100

    add product class (#1)
    Co-authored-by: Bartosz Kurto <kurtobartosz@gmail.com>

--- Sklep/Sklep/Program.cs
+++ Sklep/Sklep/Program.cs
@@ -28,6 +28,41 @@ namespace Sklep
             }
         }
 
+        class Produkt
+        {
+
+            private string nazwa;
+            private uint liczba_sztuk;
+            private double cena;
+
+            Produkt(string nazwa,uint liczba_sztuk)
+            {
+                this.nazwa = nazwa;
+                this.liczba_sztuk = liczba_sztuk;
+            }
+            public string Nazwa {
+                get { return nazwa; }
+            }
+            public double Liczba_sztuk
+            {
+                get { return liczba_sztuk; }
+            }
+
+            public bool Set_liczba_Sztuk(int o_ile_zmnienic)
+            {
+                if(liczba_sztuk-o_ile_zmnienic>0)
+                {
+                    liczba_sztuk = (uint)((int)liczba_sztuk+o_ile_zmnienic);
+                    return true;
+                }
+                else
+                {
+                    liczba_sztuk = 0;
+                    return false;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
 
