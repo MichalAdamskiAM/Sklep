a071e17b264318ea18f298e00deea893d0e4bd68
MichalAdamskiAM <adamski.michal.am@gmail.com>
2025-12-07 22:08:01 +0100

    purchase
    and feature: you can add more than one piece of product to your basket at once

--- Sklep/Sklep/obj/Debug/net7.0/Sklep.AssemblyInfo.cs
+++ Sklep/Sklep/obj/Debug/net7.0/Sklep.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sklep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d25c2d8df7c86245bd9c9dc359f4038f89d89fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa8a8d79c69a52e9332329d96a5e5a4a69ff2e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sklep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sklep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
